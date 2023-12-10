b1cbf97d46a6db8ea8636999650ffbc2c69dfe4b
zentrust1 <146930369+zentrust1@users.noreply.github.com>
2023-12-10 12:08:31 +0700

    Add files via upload

---
+++ c.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright cc 2022 anothermi
+
+# Variabel konfigurasi
+KERNEL_CONFIG="X657B_defconfig"
+CROSS_COMPILE_PREFIX="arm-linux-gnueabi-"
+
+# Pesan output yang jelas
+function init() {
+    echo "==========================="
+    echo "= START COMPILING KERNEL  ="
+    echo "==========================="
+}
+
+# Konfigurasi dan kompilasi kernel
+function compile() {
+    echo "Configuring kernel..."
+    make -j$(nproc --all) O=out ARCH=arm $KERNEL_CONFIG || { echo "Failed to configure kernel"; exit 1; }
+
+    echo "Compiling kernel..."
+    make -j$(nproc --all) ARCH=arm O=out CROSS_COMPILE=$CROSS_COMPILE_PREFIX || { echo "Kernel compilation failed"; exit 1; }
+}
+
+# Pesan output setelah selesai
+function end() {
+    echo "==========================="
+    echo "   KERNEL COMPILATION COMPLETE  "
+    echo "==========================="
+}
+
+# Eksekusi
+init
+compile
+end
